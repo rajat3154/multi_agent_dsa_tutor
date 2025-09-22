93650aa1872c8f2f90ab78c57f3ee420f667f18b
rajat3154 <rajaturanvir31@gmail.com>
2025-09-23 02:37:53 +0530

    Modified requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://multi-agent-dsa-tutor.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
